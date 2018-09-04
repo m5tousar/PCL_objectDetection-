c23b8b54f991bae7a77e8c932c625ca5a7c69e95
Tousar Mohammed <Tousar@Tousars-MacBook-Pro.local>
2018-09-04 15:53:07 -0500

    First commit

---
+++ pcl_cluster/build/CMakeFiles/find_clusters.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/find_clusters.dir/find_clusters.cpp.o"
+  "CMakeFiles/find_clusters.dir/CloudVisualizer.cpp.o"
+  "find_clusters.pdb"
+  "find_clusters"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/find_clusters.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
